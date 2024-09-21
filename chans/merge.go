8b7b87f8ad426651ef39ca588e3e39f1d90ba7e7
Andy Kreling <krelinga@gmail.com>
2024-09-21 03:09:43 +0000

    helper func for merging channels.

---
+++ chans/merge.go
@@ -0,0 +1,26 @@
+package chans
+
+import "sync"
+
+// Merge() merges multiple channels into a single channel.
+func Merge[t any](channels ...<-chan t) <-chan t {
+	out := make(chan t, len(channels))
+	var wg sync.WaitGroup
+	wg.Add(len(channels))
+	for _, c := range channels {
+		c := c
+		go func() {
+			defer wg.Done()
+			for v := range c {
+				out <- v
+			}
+		}()
+	}
+
+	go func() {
+		wg.Wait()
+		close(out)
+	}()
+
+	return out
+}
---
+++ chans/merge_test.go
@@ -0,0 +1,53 @@
+package chans
+
+import (
+	"testing"
+	"time"
+
+	"github.com/stretchr/testify/assert"
+)
+
+func TestMerge(t *testing.T) {
+	t.Parallel()
+
+	assertEventuallyClosed := func(t *testing.T, c <-chan int) {
+		assert.Eventually(t, func() bool {
+			select {
+			case _, ok := <-c:
+				return !ok
+			default:
+				return false
+			}
+		}, time.Second, 10*time.Millisecond)
+	}
+
+	t.Run("Empty", func(t *testing.T) {
+		t.Parallel()
+		c := Merge[int]()
+		assertEventuallyClosed(t, c)
+	})
+	t.Run("Single", func(t *testing.T) {
+		t.Parallel()
+		c1 := make(chan int, 1)
+		c := Merge(c1)
+		c1 <- 1024
+		close(c1)
+		assert.Equal(t, 1024, <-c)
+		assertEventuallyClosed(t, c)
+	})
+	t.Run("Multiple", func(t *testing.T) {
+		t.Parallel()
+		c1 := make(chan int, 1)
+		c2 := make(chan int, 1)
+		c := Merge(c1, c2)
+		c1 <- 1024
+		c2 <- 2048
+		close(c1)
+		close(c2)
+		found := []int{}
+		for v := range c {
+			found = append(found, v)
+		}
+		assert.ElementsMatch(t, []int{1024, 2048}, found)
+	})
+}
