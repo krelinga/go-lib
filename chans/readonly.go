be6be0d373e6a808da454c79029334c8ccd2c568
Andy Kreling <krelinga@gmail.com>
2024-09-21 03:40:53 +0000

    chans.ReadOnly()

---
+++ chans/readonly.go
@@ -0,0 +1,10 @@
+package chans
+
+// spell-checker:ignore chans
+
+// ReadOnly returns a read-only channel.
+//
+// This is useful to work around some limitations in go generics.
+func ReadOnly[t any](in chan t) <-chan t {
+	return in
+}
