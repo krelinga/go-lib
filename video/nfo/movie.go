16a72d920e6ab5d1a46d270aab4561d6df266887
Andy Kreling <krelinga@gmail.com>
2025-02-07 23:49:54 +0000

    copy & paste code from episode to movie.

--- video/nfo/movie.go
+++ video/nfo/movie.go
@@ -1,13 +1,63 @@
 package nfo
 
-import "github.com/beevik/etree"
+import (
+	"fmt"
+	"strconv"
+
+	"github.com/beevik/etree"
+)
 
 type Movie struct {
-	// TODO
+	title         *etree.Element
+	width, height *etree.Element
 }
 
 func (*Movie) validNfoSubtype() {}
 
-func parseMovie(_ *etree.Document) (*Movie, error) {
-	return &Movie{}, nil
+var (
+	pathMovieTitle  = etree.MustCompilePath("/movie/title")
+	pathMovieWidth  = etree.MustCompilePath("/movie/fileinfo/streamdetails/video/width")
+	pathMovieHeight = etree.MustCompilePath("/movie/fileinfo/streamdetails/video/height")
+)
+
+func parseMovie(doc *etree.Document) (*Movie, error) {
+	movie := &Movie{}
+
+	titles := doc.FindElementsPath(pathMovieTitle)
+	switch len(titles) {
+	case 0:
+		return nil, ErrNoTitle
+	case 1:
+		movie.title = titles[0]
+	default:
+		return nil, ErrMultipleTitles
+	}
+
+	widths := doc.FindElementsPath(pathMovieWidth)
+	switch len(widths) {
+	case 0:
+		return nil, ErrNoWidth
+	case 1:
+		movie.width = widths[0]
+		if i, err := strconv.Atoi(movie.width.Text()); err != nil || i <= 0 {
+			return nil, fmt.Errorf("%w: %s", ErrInvalidWidth, movie.width.Text())
+		}
+	default:
+		return nil, ErrMultipleWidths
+	}
+
+	heights := doc.FindElementsPath(pathMovieHeight)
+	switch len(heights) {
+	case 0:
+		return nil, ErrNoHeight
+	case 1:
+		movie.height = heights[0]
+		if i, err := strconv.Atoi(movie.height.Text()); err != nil || i <= 0 {
+			return nil, fmt.Errorf("%w: %s", ErrInvalidHeight, movie.height.Text())
+		}
+	default:
+		return nil, ErrMultipleHeights
+	}
+
+	return movie, nil
 }
---
+++ video/nfo/movie_test.go
@@ -0,0 +1,152 @@
+package nfo
+
+import (
+	"io"
+	"os"
+	"strings"
+	"testing"
+
+	"github.com/stretchr/testify/assert"
+)
+
+func TestParseMovie(t *testing.T) {
+	tests := []struct {
+		name       string
+		nfo        string
+		nfoPath    string
+		wantErr    error
+		wantTitle  string
+		wantWidth  int
+		wantHeight int
+	}{
+		{
+			name:       "Happy Path",
+			nfoPath:    "testdata/episode.nfo",
+			wantTitle:  "Asteroid Blues",
+			wantWidth:  720,
+			wantHeight: 480,
+		},
+		{
+			name:    "No title",
+			nfo:     `<movie></movie>`,
+			wantErr: ErrNoTitle,
+		},
+		{
+			name:    "Multiple titles",
+			nfo:     `<movie><title>Title 1</title><title>Title 2</title></movie>`,
+			wantErr: ErrMultipleTitles,
+		},
+		{
+			name:    "No width",
+			nfo:     `<movie><title>Title</title></movie>`,
+			wantErr: ErrNoWidth,
+		},
+		{
+			name: "Invalid width",
+			nfo: `
+			<movie>
+				<title>Title</title>
+				<fileinfo>
+					<streamdetails>
+						<video>
+							<width>invalid</width>
+						</video>
+					</streamdetails>
+				</fileinfo>
+			</movie>`,
+			wantErr: ErrInvalidWidth,
+		},
+		{
+			name: "Multiple widths",
+			nfo: `
+			<movie>
+				<title>Title</title>
+				<fileinfo>
+					<streamdetails>
+						<video>
+							<width>720</width>
+							<width>720</width>
+						</video>
+					</streamdetails>
+				</fileinfo>
+			</movie>`,
+			wantErr: ErrMultipleWidths,
+		},
+		{
+			name: "No height",
+			nfo: `
+			<movie>
+				<title>Title</title>
+				<fileinfo>
+					<streamdetails>
+						<video>
+							<width>720</width>
+						</video>
+					</streamdetails>
+				</fileinfo>
+			</movie>`,
+			wantErr: ErrNoHeight,
+		},
+		{
+			name: "Invalid height",
+			nfo: `
+			<movie>
+				<title>Title</title>
+				<fileinfo>
+					<streamdetails>
+						<video>
+							<width>720</width>
+							<height>invalid</height>
+						</video>
+					</streamdetails>
+				</fileinfo>
+			</movie>`,
+			wantErr: ErrInvalidHeight,
+		},
+		{
+			name: "Multiple heights",
+			nfo: `
+			<movie>
+				<title>Title</title>
+				<fileinfo>
+					<streamdetails>
+						<video>
+							<width>720</width>
+							<height>480</height>
+							<height>480</height>
+						</video>
+					</streamdetails>
+				</fileinfo>
+			</movie>`,
+			wantErr: ErrMultipleHeights,
+		},
+	}
+
+	for _, tt := range tests {
+		t.Run(tt.name, func(t *testing.T) {
+			var r io.Reader
+			if tt.nfoPath != "" {
+				var err error
+				r, err = os.Open(tt.nfoPath)
+				if err != nil {
+					t.Fatalf("Failed to open file: %v", err)
+				}
+			} else {
+				r = strings.NewReader(tt.nfo)
+			}
+			out, err := Parse(r)
+			assert.ErrorIs(t, err, tt.wantErr)
+			if err != nil {
+				return
+			}
+
+			outEpisode, ok := out.(*Episode)
+			if !ok {
+				t.Fatal("Wrong type returned")
+			}
+			assert.Equal(t, tt.wantTitle, outEpisode.Title(), "title")
+			assert.Equal(t, tt.wantWidth, outEpisode.Width(), "width")
+			assert.Equal(t, tt.wantHeight, outEpisode.GetHeight(), "height")
+		})
+	}
+}
