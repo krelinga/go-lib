df9daa48770aadf518725ffef7712a3ad9123218
Andy Kreling <krelinga@gmail.com>
2025-02-25 02:44:39 +0000

    tests for single depth error reports.

--- ops/validateops/validateopsmock/sink_test.go
+++ ops/validateops/validateopsmock/sink_test.go
@@ -16,12 +16,44 @@ func TestSink(t *testing.T) {
 		want []validateopsmock.Entry
 	}{
 		{
+			name: "no errors",
+			setup: func(s *validateopsmock.Sink) {},
+			want: nil,
+		},
+		{
 			name: "single root error",
 			setup: func(s *validateopsmock.Sink) {
 				s.Error(wantError)
 			},
 			want: []validateopsmock.Entry{
 				{Err: wantError},
+			},
+		},
+		{
+			name: "single field error",
+			setup: func(s *validateopsmock.Sink) {
+				s.Field("foo").Error(wantError)
+			},
+			want: []validateopsmock.Entry{
+				{Context: "foo", Err: wantError},
+			},
+		},
+		{
+			name: "single key error",
+			setup: func(s *validateopsmock.Sink) {
+				s.Key(10).Error(wantError)
+			},
+			want: []validateopsmock.Entry{
+				{Context: "[10]", Err: wantError},
+			},
+		},
+		{
+			name: "single note error",
+			setup: func(s *validateopsmock.Sink) {
+				s.Note("note").Error(wantError)
+			},
+			want: []validateopsmock.Entry{
+				{Context: "(note)", Err: wantError},
 			},
 		},
 	}
