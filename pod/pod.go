d43ab493388b13f2966f125259c32cdf905ca81e
Andy Kreling <krelinga@gmail.com>
2025-03-02 01:25:17 +0000

    remove earlier generic validation, diffing, and deep copy attempts.

--- pod/pod.go
+++
@@ -1,9 +0,0 @@
-package pod
-
-type POD interface {
-	InternalValidate(reporter ErrorReporter)
-	InternalDiff(rhs POD, reporter DiffReporter)
-	// TODO: rename this to InternalCopyTo ... not all copies will be deep.
-	// TODO: ditch DeepCopyTo, and just have DeepCopy return a new copy.
-	InternalDeepCopyTo(out POD)
-}
--- podreflect/podmock/error_reporter.go
+++
@@ -1,9 +0,0 @@
-package podmock
-
-type ErrorReporter struct {
-	Errs []error
-}
-
-func (er *ErrorReporter) Err(err error) {
-	er.Errs = append(er.Errs, err)
-}
