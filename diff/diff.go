de41d7683625ee61a8af71c647161f86a54e72ee
Andy Kreling <krelinga@gmail.com>
2025-03-03 03:40:17 +0000

    diff pointers by dereferenced value (recursively).
    report an error if any non-diffable values are passed to Diff().

--- diff/diff.go
+++ diff/diff.go
@@ -1,5 +1,50 @@
 package diff
 
-func Diff[T comparable](lhs, rhs T) bool {
-	return lhs != rhs
+import (
+	"errors"
+	"reflect"
+)
+
+var ErrUnsupportedType = errors.New("unsupported type")
+
+func Diff[T any](lhs, rhs T) (bool, error) {
+	return diffWithReflection(lhs, rhs)
+}
+
+func diffWithReflection(lhs, rhs any) (bool, error) {
+	lhsType := reflect.TypeOf(lhs)
+	rhsType := reflect.TypeOf(rhs)
+	if lhsType != rhsType {
+		return true, nil
+	}
+	
+	if lhsType.Kind() == reflect.Pointer {
+		return diffPointer(lhs, rhs)
+	} else if lhsType.Comparable() {
+		return diffComparable(lhs, rhs), nil
+	}
+
+	return false, ErrUnsupportedType
+}
+
+func valueOk(value reflect.Value) bool {
+	return value.IsValid() && !value.IsNil()
+}
+
+func diffPointer(lhs, rhs any) (bool, error) {
+	lhsValue := reflect.ValueOf(lhs)
+	rhsValue := reflect.ValueOf(rhs)
+	lhsOk := valueOk(lhsValue)
+	rhsOk := valueOk(rhsValue)
+	if !lhsOk && !rhsOk {
+		return false, nil
+	} else if lhsOk && rhsOk {
+		return diffWithReflection(lhsValue.Elem().Interface(), rhsValue.Elem().Interface())
+	}
+
+	return true, nil
+}
+
+func diffComparable(lhs, rhs any) bool {
+	return rhs != lhs
 }
--- diff/diff_test.go
+++ diff/diff_test.go
@@ -6,28 +6,46 @@ type runner interface {
 	run(t *testing.T)
 }
 
-type testDiffCase[T comparable] struct {
+type testDiffCase[T any] struct {
 	name string
 	lhs  T
 	rhs  T
 	want bool
+	wantErr error
 }
 
 func (c testDiffCase[T]) run(t *testing.T) {
-	t.Helper()
-	if got := Diff(c.lhs, c.rhs); got != c.want {
-		t.Errorf("Diff() = %v, want %v", got, c.want)
-	}
+	t.Run(c.name, func(t *testing.T) {
+		got, gotErr := Diff(c.lhs, c.rhs)
+		if gotErr != c.wantErr {
+			t.Errorf("Diff() error = %v, want %v", gotErr, c.wantErr)
+		} else if got != c.want {
+			t.Errorf("Diff() = %v, want %v", got, c.want)
+		}
+	})
+}
+
+func ptr[T any](v T) *T {
+	return &v
 }
 
 func TestDiff(t *testing.T) {
 	tests := []runner{
 		testDiffCase[int]{name: "int not equal", lhs: 1, rhs: 2, want: true},
 		testDiffCase[int]{name: "int equal", lhs: 1, rhs: 1, want: false},
+		testDiffCase[*int]{name: "int ptr not equal", lhs: ptr(1), rhs: ptr(2), want: true},
+		testDiffCase[*int]{name: "int ptr equal", lhs: ptr(1), rhs: ptr(1), want: false},
+		testDiffCase[**int]{name: "int ptr ptr not equal", lhs: ptr(ptr(1)), rhs: ptr(ptr(2)), want: true},
+		testDiffCase[**int]{name: "int ptr ptr equal", lhs: ptr(ptr(1)), rhs: ptr(ptr(1)), want: false},
 		testDiffCase[string]{name: "string not equal", lhs: "a", rhs: "b", want: true},
 		testDiffCase[string]{name: "string equal", lhs: "a", rhs: "a", want: false},
+		testDiffCase[*string]{name: "string ptr not equal", lhs: ptr("a"), rhs: ptr("b"), want: true},
+		testDiffCase[*string]{name: "string ptr equal", lhs: ptr("a"), rhs: ptr("a"), want: false},
 		testDiffCase[float64]{name: "float64 not equal", lhs: 1.0, rhs: 2.0, want: true},
 		testDiffCase[float64]{name: "float64 equal", lhs: 1.0, rhs: 1.0, want: false},
+		testDiffCase[*float64]{name: "float64 ptr not equal", lhs: ptr(1.0), rhs: ptr(2.0), want: true},
+		testDiffCase[*float64]{name: "float64 ptr equal", lhs: ptr(1.0), rhs: ptr(1.0), want: false},
+		testDiffCase[map[int]int]{name: "map not supported", lhs: nil, rhs: nil, wantErr: ErrUnsupportedType},
 	}
 	for _, tt := range tests {
 		tt.run(t)
