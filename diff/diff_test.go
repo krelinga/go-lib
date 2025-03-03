9997041516a73a5837ffbbdd8e367bb459413393
Andy Kreling <krelinga@gmail.com>
2025-03-03 03:45:22 +0000

    test that slice is not currenty supported.

--- diff/diff_test.go
+++ diff/diff_test.go
@@ -7,10 +7,10 @@ type runner interface {
 }
 
 type testDiffCase[T any] struct {
-	name string
-	lhs  T
-	rhs  T
-	want bool
+	name    string
+	lhs     T
+	rhs     T
+	want    bool
 	wantErr error
 }
 
@@ -46,6 +46,7 @@ func TestDiff(t *testing.T) {
 		testDiffCase[*float64]{name: "float64 ptr not equal", lhs: ptr(1.0), rhs: ptr(2.0), want: true},
 		testDiffCase[*float64]{name: "float64 ptr equal", lhs: ptr(1.0), rhs: ptr(1.0), want: false},
 		testDiffCase[map[int]int]{name: "map not supported", lhs: nil, rhs: nil, wantErr: ErrUnsupportedType},
+		testDiffCase[[]int]{name: "slice not supported", lhs: nil, rhs: nil, wantErr: ErrUnsupportedType},
 	}
 	for _, tt := range tests {
 		tt.run(t)
