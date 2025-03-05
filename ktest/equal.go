6397713bdefdc1cbb2a1435aa0f98626c75ec3d6
Andy Kreling <krelinga@gmail.com>
2025-03-05 17:03:49 +0000

    first test that leverages diff.Diff().

---
+++ ktest/equal.go
@@ -0,0 +1,41 @@
+package ktest
+
+import (
+	"fmt"
+	"testing"
+
+	"github.com/krelinga/go-lib/diff"
+)
+
+const fmtDifferent = `
+- %s: %v
++ %s: %v
+`
+
+const fmtMissing = `
+- %s: %v
+`
+
+const fmtExtra = `
++ %s: %v
+`
+
+// Returns true if lhs and rhs are equal, false otherwise.
+func AssertEqual[T any](t *testing.T, lhs, rhs T) bool {
+	t.Helper()
+	diffResult := diff.Diff(lhs, rhs)
+	if diffResult == nil {
+		return true
+	}
+	switch diffResult.Kind {
+	case diff.Different:
+		t.Errorf(fmtDifferent, diffResult.Path, diffResult.Lhs, diffResult.Path, diffResult.Rhs)
+	case diff.Missing:
+		t.Errorf(fmtMissing, diffResult.Path, diffResult.Lhs)
+	case diff.Extra:
+		t.Errorf(fmtExtra, diffResult.Path, diffResult.Rhs)
+	default:
+		panic(fmt.Sprintf("unexpected diff kind: %v", diffResult.Kind))
+	}
+	return false
+}
